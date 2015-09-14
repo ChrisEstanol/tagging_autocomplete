9d51593a1bb1a12f7377c6eb438436cb927cb8b3
Christophe E <cestanol@gmail.com>
2015-09-13 21:52:09 -0500

    Bootstrap views & error messages

--- Gemfile
+++ Gemfile
@@ -14,6 +14,7 @@ gem 'sdoc', '~> 0.4.0', group: :doc
 
 gem 'bootstrap-sass'
 gem 'acts-as-taggable-on'
+gem 'rails-assets-chosen'
 
 
 group :development, :test do
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150913234000) do
+ActiveRecord::Schema.define(version: 20150914002809) do
 
   create_table "products", force: :cascade do |t|
     t.string   "name"
     t.text     "description"
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
+  end
+
+  create_table "tags", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
