32def8479736c41e3a88a794fee9d50464680280
Christophe E <cestanol@gmail.com>
2015-09-15 15:37:50 -0500

    Filter by tag

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -32,7 +32,7 @@ class ProductsController < ApplicationController
 
     respond_to do |format|
       if @product.save
-        format.html { redirect_to @product, notice: 'Product was successfully created.' }
+        format.html { redirect_to products_path, notice: 'Product was successfully created.' }
         format.json { render :show, status: :created, location: @product }
       else
         format.html { render :new }
@@ -46,7 +46,7 @@ class ProductsController < ApplicationController
   def update
     respond_to do |format|
       if @product.update(product_params)
-        format.html { redirect_to @product, notice: 'Product was successfully updated.' }
+        format.html { redirect_to products_path, notice: 'Product was successfully updated.' }
         format.json { render :show, status: :ok, location: @product }
       else
         format.html { render :edit }
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  resources :tags
-  # get 'tags/:tag', to: 'people#index', as: :tag
-  resources :products
   root 'products#index'
+  resources :tags  
+  resources :products
+  get 'tags/:tag', to: 'products#index'
 end
